ee4de990d78e522efbd12121df9c0ab1b72f746e
baekhun <baekhun2463@gmail.com>
2024-07-16 12:59:32 +0900

    Splash 화면 만드는 중

--- CoinAlert/ContentView.swift
+++ CoinAlert/ContentView.swift
@@ -7,21 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View{
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("Hello, wordl!")
-            Text("Hello, wordl!")
-
+        VStack{
+            Text("메인 화면임")
+                .font(.largeTitle)
+                .padding()
         }
-        .padding()
+        .background(Color.white)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
-#Preview {
-    ContentView()
+struct SplashView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            //로고 들어갈 자리
+            Text("이미지 들어갈 자리")
+                .font(.largeTitle)
+            Spacer()
+        }
+        .background(Color.white)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
 }
+
+struct ContentView: View {
+    
+    @State private var showMainView = false
+    
+    func loadData() {
+        DispatchQueue.global().async {
+            sleep(1)
+            
+            DispatchQueue.main.async {
+                showMainView = true
+            }
+        }
+    }
+    
+    var body: some View {
+        Group {
+            if showMainView {
+                MainView()
+            } else {
+                SplashView()
+                    .onAppear {
+                        loadData()
+                    }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
