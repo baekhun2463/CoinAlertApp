74dc7dfd224cbbe6c5012a97e878c650f0bb3816
baekhun <baekhun2463@gmail.com>
2024-07-16 11:59:14 +0900

    Initial Commit

---
+++ CoinAlert/CoinAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinAlertApp.swift
+//  CoinAlert
+//
+//  Created by 백지훈 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinAlert
+//
+//  Created by 백지훈 on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
