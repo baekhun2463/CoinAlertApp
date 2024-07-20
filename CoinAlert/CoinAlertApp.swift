7cfd13dc2aec05d759f1b852ef787bd78112dc22
baekhun <baekhun2463@gmail.com>
2024-07-20 18:27:29 +0900

    swiftdata 에러 수정 완료 (로직 에러남)

--- CoinAlert/CoinAlertApp.swift
+++ CoinAlert/CoinAlertApp.swift
@@ -18,6 +18,9 @@ struct CoinAlertApp: App {
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
+        // Remove existing persistent store file if exists
+        removePersistentStore()
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
@@ -33,4 +36,15 @@ struct CoinAlertApp: App {
     }
 }
 
+func removePersistentStore() {
+    let storeURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first?.appendingPathComponent("default.store")
 
+    if let storeURL = storeURL {
+        do {
+            try FileManager.default.removeItem(at: storeURL)
+            print("Persistent store deleted.")
+        } catch {
+            print("Failed to delete persistent store: \(error)")
+        }
+    }
+}
--- CoinAlert/Models/PriceData.swift
+++ CoinAlert/Models/PriceData.swift
@@ -5,17 +5,17 @@
 //  Created by 백지훈 on 7/17/24.
 //
 
-
 import Foundation
 import SwiftData
 
 @Model
-class PriceData {
-    @Attribute(.unique) var id = UUID()
-    var price: Double
-    var date: Date
+final class PriceData {
+    @Attribute(.unique) var id: UUID
+    @Attribute var price: Double
+    @Attribute var date: Date
     
     init(price: Double, date: Date) {
+        self.id = UUID()
         self.price = price
         self.date = date
     }
